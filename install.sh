a3d2c9df8c61239dd3e7c125376c284e4f790e81
Andrew Krause <ajkrause@gmail.com>
2022-11-05 23:18:36 -0400

    Add color scheme

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/simple-dark.vim ~/.vim/colors/simple-dark.vim
  
